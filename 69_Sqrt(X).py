b370015d74592de3ccf55a29e50c58296a39d727
NyxSkye <3dbuilder308@gmail.com>
2026-08-20 13:39:33 +0530

    Ques 6. Date: 19:08:2026

--- 69_Sqrt(X).py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def mySqrt(self, x):
-        ans=0
-        for i in range(0,x+1):
-            if i*i<=x:
-                ans=i
-            else:
-                break
-        return ans
