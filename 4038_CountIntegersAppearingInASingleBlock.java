58d0db49cac60f4e4f066f1e066f9c801d49f426
NyxAurionSkye <3dbuilder308@gmail.com>
2026-08-31 17:35:53 +0530

    Ques 1. Date: 30:08:2026

---
+++ 4038_CountIntegersAppearingInASingleBlock.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countSpecialIntegers(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        Set<Integer> invalid = new HashSet<>();
+        int prev = -1;
+        for (int num : nums) {
+            if (num != prev) {
+                if (seen.contains(num)) {
+                    invalid.add(num);
+                }
+                seen.add(num);
+            }
+            prev = num;
+        }
+        return seen.size() - invalid.size();
+    }
+}
